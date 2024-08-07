e8ad352c20b9a068a71eb2ccf0c599f81efc4954
Manthanjadhav <jadhavmanthan33@gmail.com>
2024-08-07 19:34:24 +0530

    Express and cors setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv';
 
@@ -5,8 +6,19 @@ dotenv.config({
     path:"./env"
 })
 
-
+const port=process.env.PORT;
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR",error);
+    })
+    app.listen(port||8000,()=>{
+        console.log(`Server is running at the port ${port}`)
+    });
+})
+.catch((err)=>{
+    console.log("MONGODB Connection error : ",err)
+})
 
 
 
